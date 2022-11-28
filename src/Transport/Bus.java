5414403ab5727ee4df7c6dd262f62a9c35dae37b
Konstantin <kdanilov1911@gmail.com>
2022-11-28 16:08:44 +0300

    HW is done

--- src/Transport/Bus.java
+++ src/Transport/Bus.java
@@ -1,9 +1,44 @@
 package Transport;
 
 public class Bus extends Transport implements Challenge {
+    public enum CapacityType {
+        EXTRA_SMALL(10),
+        SMALL(25),
+        MEDIUM_DOWN_BOUND(40),
+        MEDIUM_UPPER_BOUND(50),
+        LARGE_DOWN_BOUND(60),
+        LARGE_UPPER_BOUND(80),
+        ESPECIALLY_LARGE_DOWN_BOUND(100),
+        ESPECIALLY_LARGE_UPPER_BOUND(120);
 
-    public Bus(String brand, String model, double engineVolume) {
+        int capacityType;
+
+        CapacityType(int capacityType) {
+            this.capacityType = capacityType;
+        }
+
+        @Override
+        public String toString() {
+            return " " + capacityType;
+        }
+    }
+
+    CapacityType capacityType;
+    CapacityType capacityTypeTwo;
+
+    private TransportType transportType;
+
+    public Bus(String brand, String model, double engineVolume, CapacityType downBoundCapacityType,
+               CapacityType upperBoundCapacityType) {
         super(brand, model, engineVolume);
+        this.capacityType = downBoundCapacityType;
+        this.capacityTypeTwo = upperBoundCapacityType;
+
+    }
+
+    public Bus(String brand, String model, double engineVolume, CapacityType capacityType) {
+        super(brand, model, engineVolume);
+        this.capacityType = capacityType;
     }
 
     @Override
@@ -33,10 +68,36 @@ public class Bus extends Transport implements Challenge {
 
     @Override
     public String toString() {
-        return "Bus{" +
-                "brand='" + getBrand() + '\'' +
-                ", model='" + getModel() + '\'' +
-                "}";
+        if (capacityTypeTwo == null) {
+            return "Bus{" +
+                    "brand='" + getBrand() + '\'' +
+                    ", model='" + getModel() + '\'' +
+                    "Вместимость:" + capacityType + " мест" +
+                    "}";
+        } else {
+            return "Bus{" +
+                    "brand='" + getBrand() + '\'' +
+                    ", model='" + getModel() + '\'' +
+                    "Вместимость:" + capacityType + " -"
+                    + capacityTypeTwo + " мест" +
+                    "}";
+        }
     }
 
+    @Override
+    public void printType() {
+        if (transportType == null) {
+            System.out.println("Данных по транспортному средству недостаточно");
+        } else {
+            System.out.println(transportType);
+        }
+    }
+
+    public TransportType getTransportType() {
+        return transportType;
+    }
+
+    public void setTransportType(TransportType transportType) {
+        this.transportType = transportType;
+    }
 }
--- src/Transport/Trucks.java
+++ src/Transport/Trucks.java
@@ -1,9 +1,40 @@
 package Transport;
 
 public class Trucks extends Transport implements Challenge {
+    public enum LoadCapacity {
+        N1("до 3,5 тонн"),
+        N2_DOWN_BOUND("от 3,5 тонн"),
+        N2_UPPER_BOUND("до 12 тонн"),
+        N3("свыше 12 тонн");
 
-    public Trucks(String brand, String model, double engineVolume) {
+        String loadCapacity;
+
+        LoadCapacity(String loadCapacity) {
+            this.loadCapacity = loadCapacity;
+        }
+
+        @Override
+        public String toString() {
+            return loadCapacity;
+        }
+    }
+
+    LoadCapacity loadCapacity;
+    LoadCapacity loadCapacityTwo;
+
+    private TransportType transportType;
+
+    public Trucks(String brand, String model, double engineVolume, LoadCapacity downBoundLoadCapacity,
+                  LoadCapacity upperBoundLoadCapacity) {
         super(brand, model, engineVolume);
+        this.loadCapacity = downBoundLoadCapacity;
+        this.loadCapacityTwo = upperBoundLoadCapacity;
+
+    }
+
+    public Trucks(String brand, String model, double engineVolume, LoadCapacity loadCapacity) {
+        super(brand, model, engineVolume);
+        this.loadCapacity = loadCapacity;
     }
 
     @Override
@@ -34,9 +65,36 @@ public class Trucks extends Transport implements Challenge {
     @Override
     public String toString() {
 
-        return "Track{" +
-                "brand='" + getBrand() + '\'' +
-                ", model='" + getModel() + '\'' +
-                '}';
+        if (this.loadCapacityTwo == null) {
+            return "Track{" +
+                    "brand='" + getBrand() + '\'' +
+                    ", model='" + getModel() + '\'' +
+                    "Грузоподъемность: " + loadCapacity +
+                    '}';
+        } else {
+            return "Track{" +
+                    "brand='" + getBrand() + '\'' +
+                    ", model='" + getModel() + '\'' +
+                    "Грузоподъемность: " + loadCapacity +
+                    " " + loadCapacityTwo +
+                    '}';
+        }
+    }
+
+    @Override
+    public void printType() {
+        if (transportType == null) {
+            System.out.println("Данных по транспортному средству недостаточно");
+        } else {
+            System.out.println(transportType);
+        }
+    }
+
+    public TransportType getTransportType() {
+        return transportType;
+    }
+
+    public void setTransportType(TransportType transportType) {
+        this.transportType = transportType;
     }
 }
