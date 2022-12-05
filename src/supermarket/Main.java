2ea3ae546a2cb87a9ed18d35c11f5e5ae5b22334
Konstantin <kdanilov1911@gmail.com>
2022-12-06 01:41:31 +0300

    HW is done

---
+++ src/supermarket/Main.java
@@ -0,0 +1,34 @@
+package supermarket;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Supermarket supermarket = new Supermarket();
+
+        String one = "Петя";
+        String two = "Иван";
+        String three = "Вася";
+        String four = "Нина";
+        String five = "Таня";
+        String six = "Маша";
+        String seven = "Руслан";
+        String eight = "Оля";
+        String nine = "Марина";
+        String ten = "Костя";
+        String eleven = "Руслан";
+        String twelve = "Руслан";
+
+        supermarket.add(one);
+        supermarket.add(two);
+        supermarket.add(three);
+        supermarket.add(four);
+        supermarket.add(five);
+        supermarket.add(six);
+        supermarket.add(seven);
+        supermarket.add(eight);
+        supermarket.add(nine);
+        supermarket.add(ten);
+        supermarket.add(eleven);
+        supermarket.add(twelve);
+    }
+}
